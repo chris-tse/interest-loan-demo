75d42e9ce63ac071f0b39f9ae5b3df1584343c57
Christopher Tse <chris@chris-tse.com>
2020-09-02 23:03:54 -0500

    Make tweaks to UI and add JS for calculating final amount

--- js/index.js
+++ js/index.js
@@ -1,14 +1,43 @@
 // Get element references
 const slider = document.querySelector('#initial-amount')
 const sliderDisplay = document.querySelector('#initial-amount-display .amount')
+const interestRateInput = document.querySelector('#interest-rate')
+const yearInput = document.querySelector('#years')
+const calculateButton = document.querySelector('.calculate-button')
+const monthlyRadio = document.querySelector('#monthly')
+const yearlyRadio = document.querySelector('#yearly')
 
-const currencyFormatter = new Intl.NumberFormat('en-US')
+const numberFormatter = new Intl.NumberFormat('en-US')
 
 // Initial startup
-sliderDisplay.innerHTML = currencyFormatter.format(slider.value)
+sliderDisplay.innerHTML = numberFormatter.format(slider.value)
+calculateFinalAmount()
 
 // Add listeners
 slider.addEventListener('input', handleSlide)
+calculateButton.addEventListener('click', calculateFinalAmount)
+
+/**
+ * Calculates compound interest with formula A = P(1 + r/n)^tn where
+ * A - final amount
+ * P - principal amount
+ * r - interest rate in decimal (e.g. 2% -> 0.02)
+ * n - Times compounded per time period
+ * t - Time period in years
+ */
+function calculateFinalAmount() {
+	const initialAmount = slider.value
+	// Convert interest rate from percentage to decimal
+	const interestRate = interestRateInput.value / 100
+	const years = yearInput.value
+	// 12 or 1 compounding periods depending if monthly or yearly
+	const compoundPerYear = monthlyRadio.checked ? 12 : 1
+	const finalAmount = initialAmount * Math.pow(1 + interestRate / compoundPerYear, years * compoundPerYear)
+
+	document.querySelector('#final-amount .amount').innerHTML = numberFormatter.format(Math.floor(finalAmount))
+	document.querySelector('.num-years').innerHTML = years
+	document.querySelector('.plural').innerHTML = years > 1 ? 's' : ''
+}
 
 // Define handlers
 
@@ -17,5 +46,5 @@ slider.addEventListener('input', handleSlide)
  * @param {Event} event Slider change event
  */
 function handleSlide(event) {
-	sliderDisplay.innerHTML = currencyFormatter.format(event.target.value)
+	sliderDisplay.innerHTML = numberFormatter.format(event.target.value)
 }
